2e62fb729a4457dd55cd81f8349710c41ca6335c
PROFESSEURMDX <professeurmdx@gmail.com>
2025-06-25 16:44:27 -0400

    Update thanks-to.js

--- plugins/thanks-to.js
+++ plugins/thanks-to.js
@@ -1,25 +1,7 @@
 const { cmd } = require('../command');
 const config = require('../config');
 
-cmd({
-    pattern: "ping2",
-    alias: ["thanksto"],
-    desc: "thanks to dev for helping",
-    category: "main",
-    react: "🗯️",
-    filename: __filename
-},
-async (conn, mek, m, { from }) => {
-    try {
-        const message =`╭━━━⪨𝗛𝗔𝗜𝗞𝗢-𝗠𝗗𝗫⪩━━━╮
-┃╭╼━━━━━━━━━━━┈⊷
-┃┃♦ 𝗣𝗜𝗡𝗚𝟮: *${ping}MS*
-┃┃♦︎ 𝗗𝗘𝗩: 𝗫𝗧𝗥𝗘𝗠𝗘
-┃╰╼━━━━━━━━━━━┈⊷
-╰╼══════════════╾╯
-> *𝑃𝑂𝑊𝐸𝑅𝐸𝐷 𝐵𝑌 𝑋𝑇𝑅𝐸𝑀𝐸*`;
-
-        await conn.sendMessage(from, {
+await conn.sendMessage(from, {
             image: { url: 'https://files.catbox.moe/2vosmn.jpg' },
             caption: message,
             contextInfo: {
@@ -31,11 +13,31 @@ async (conn, mek, m, { from }) => {
                     newsletterName: '𝐏𝐑𝐎𝐅-𝐗𝐓𝐑𝐄𝐌𝐄',
                     serverMessageId: 143
                 }
-            }
-        }, { quoted: mek });
 
-    } catch (err) {
-        console.error("ThanksTo Error:", err);
-        await conn.sendMessage(from, { text: `Error: ${err.message}` }, { quoted: mek });
+// ping2 
+
+cmd({
+    pattern: "ping2",
+    desc: "Check bot's response time.",
+    category: "main",
+    react: "📡",
+    filename: __filename
+},
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        const startTime = Date.now()
+        const message = await conn.sendMessage(from, { text: '*PINGING...⏳*' })
+        const endTime = Date.now()
+        const ping = endTime - startTime
+        await conn.sendMessage(from, { text: `╭━━━⪨𝗛𝗔𝗜𝗞𝗢-𝗠𝗗𝗫⪩━━━╮
+┃╭╼━━━━━━━━━━━┈⊷
+┃┃♦ 𝗣𝗜𝗡𝗚𝟮: *${ping}MS*
+┃┃♦︎ 𝗗𝗘𝗩: 𝗫𝗧𝗥𝗘𝗠𝗘
+┃╰╼━━━━━━━━━━━┈⊷
+╰╼══════════════╾╯
+> *𝑃𝑂𝑊𝐸𝑅𝐸𝐷 𝐵𝑌 𝑋𝑇𝑅𝐸𝑀𝐸*` }, { quoted: message })
+    } catch (e) {
+        console.log(e)
+        reply(`${e}`)
     }
-});
+})
