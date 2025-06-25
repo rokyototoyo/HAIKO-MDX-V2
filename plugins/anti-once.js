ea2601a4c6179a87cf83ce6ecb947c848cf6e5f2
PROFESSEURMDX <professeurmdx@gmail.com>
2025-06-25 00:30:52 -0400

    Update anti-once.js

--- plugins/anti-once.js
+++ plugins/anti-once.js
@@ -5,7 +5,6 @@ cmd({
   alias: ["waouh", "ohh", "oh", "🤤", "nice", "ok"],
   desc: "Owner Only - retrieve quoted message back to user",
   category: "owner",
-  react: "🤲🏻",
   filename: __filename
 }, async (client, message, match, { from, isCreator }) => {
   try {
